340dc935aa9f716d4942a643cb6a30a2d3f7d67f
Momonyaro <58791263+Momonyaro@users.noreply.github.com>
2023-08-08 22:40:11 +0200

    added more animations (walk, cheer)

--- Models/little_guy.gd
+++ Models/little_guy.gd
@@ -1,5 +1,20 @@
 extends Node3D
 
+const anims : Array[String] = ["IDLE", "WALK", "CHEER"];
+
+
+@export var change_anim_time : float = 2.0;
+var _timer = 0;
+var _index = 0;
+
 func _ready():
-	$AnimationPlayer.play("ArmatureAction");
+	$AnimationPlayer.play(anims[_index]);
 	pass
+
+func _process(delta):
+	_timer += delta;
+	
+	if _timer >= change_anim_time or not $AnimationPlayer.is_playing():
+		_timer = 0;
+		_index = (_index + 1) % anims.size();
+		$AnimationPlayer.play(anims[_index], 0.25);
