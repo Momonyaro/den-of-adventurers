6fe77bc0e7a0c7abdf1de04c06f4ba1c6b30caa7
Sebastian "Momonyaro <sebastian.alkstrand@gmail.com>
2023-08-16 23:01:24 +0200

    touched up the handling of agent animations

--- Scripts/Test/AgentTest.gd
+++ Scripts/Test/AgentTest.gd
@@ -8,17 +8,22 @@ extends CharacterBody3D
 @export var move_speed = 5.0;
 @export var idle_time = 2.5;
 var timer = 0;
-var current_anim = "WALK";
+var current_anim = "CHEER";
+var anim_player = null;
+
+func _ready():
+	anim_player = model.find_child("AnimationPlayer");
+	anim_player.play("CHEER", 0.1);
 
 func _process(delta):
+	if current_anim == "CHEER":
+		pass;
+	
 	if nav_agent.is_navigation_finished():
 		timer += delta;
 		if timer >= idle_time:
 			timer = 0;
 			update_target_pos(get_random_pos());
-	elif current_anim != "WALK":
-		current_anim = "WALK";
-		model.find_child("AnimationPlayer").play("WALK", 0.1);
 
 func _physics_process(delta):
 	var pos = global_transform.origin;
@@ -26,13 +31,20 @@ func _physics_process(delta):
 	if nav_agent.distance_to_target() > .1 and next_pos != pos:
 		look_at(next_pos, Vector3.UP);
 		self.rotate_object_local(Vector3.UP, PI);
-	var new_velocity = (next_pos - pos).normalized() * move_speed;
+	var new_velocity: Vector3 = (next_pos - pos).normalized() * move_speed;
 	
-	if pos.distance_to(next_pos) < .1:
-		if current_anim != "IDLE":
-			current_anim = "IDLE";
-			model.find_child("AnimationPlayer").play("IDLE", 0.1);
+	if pos.distance_to(next_pos) < .1 and current_anim != "CHEER":
 		new_velocity = Vector3.ZERO;
+	
+	if current_anim == "CHEER" and anim_player.is_playing():
+		pass;
+	elif new_velocity.length() > 0:
+		try_set_anim("WALK");
+		anim_player.play(current_anim, 0.1);
+	else:
+		try_set_anim("IDLE");
+		anim_player.play(current_anim, 0.1);
+	
 	
 	velocity = new_velocity;
 	move_and_slide();
@@ -47,3 +59,11 @@ func get_random_pos() -> Vector3:
 	var rand_point = start_point + Vector3(rand_x, 0, rand_z);
 	
 	return rand_point;
+
+func try_set_anim(name: String) -> bool:
+	if name == "CHEER" and model.find_child("AnimationPlayer").is_playing():
+		return false;
+	if name == current_anim: 
+		return false;
+	current_anim = name;
+	return true;
