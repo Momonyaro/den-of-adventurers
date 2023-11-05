bbdaef44aa21f9d520515cc703485d0d2dd5752c
Momonyaro <58791263+Momonyaro@users.noreply.github.com>
2023-11-05 21:05:27 +0100

    added window resizing and adv_preview fatigue and hp

--- Scripts/UI/windows/adv_preview.gd
+++ Scripts/UI/windows/adv_preview.gd
@@ -14,12 +14,21 @@ func _ready():
 	_game_manager.select_agent.connect(update_menu);
 	pass;
 
-func _process(delta):
+func _process(_delta):
 	update_menu();
 
 func update_menu():
+	if _selected_agent == null || _selected_agent.adventurer == null:
+		return;
+	
 	_draw_basic_info();
-	#_window_base.resize_app();
+	_draw_health_info();
+
+	var list_size = get_child(0).get_global_rect().size;
+	var base_size = self.get_global_rect().size
+	if list_size != base_size:
+		self.set_size(list_size);
+		_window_base.resize_app();
 
 func _draw_basic_info():
 	var adventurer = _selected_agent.adventurer;
@@ -28,14 +37,23 @@ func _draw_basic_info():
 	
 	get_node("%BaseInfoRect/name").text = adventurer.adv_name();
 	get_node("%BaseInfoRect/level_race").text = str("Level ", adventurer._level, " ", adventurer.adv_race());
-	get_node("%BaseInfoRect/state").text = adventurer.adv_status();
+	get_node("%BaseInfoRect/state").text = current_status;
 	get_node("%BaseInfoRect/xp_bar").visible = current_status != "RECRUIT";
 	get_node("%BaseInfoRect/xp_bar").value = xp_ratio;
 	get_node("%BaseInfoRect/recruit_btn").visible = current_status == 'RECRUIT';
 	get_node("%BaseInfoRect/recruit_btn").disabled = !_check_capacity();
 	#get_node("%BaseInfoRect/recruit_btn").tooltip_text = "You guild can't house any more recruits!" if !_check_capacity() else "Recruit a new adventurer to your guild";
-	
-	pass;
+
+func _draw_health_info():
+	var adventurer = _selected_agent.adventurer;
+	var current_status = adventurer.adv_status();
+
+	get_node("%AdvHealthRect").visible = current_status != "RECRUIT";
+	get_node("%AdvHealthRect/health").text = str("Health: (", adventurer._health.x, "/", adventurer._health.y, ")");
+	get_node("%AdvHealthRect/health/bar").value = adventurer._health.x / float(adventurer._health.y);
+	get_node("%AdvHealthRect/fatigue").text = str("Fatigue: (", "%0.0f" % (adventurer._fatigue / 1.0 * 100), "%)");
+	get_node("%AdvHealthRect/fatigue/bar").value = adventurer._fatigue / 1.0;
+
 
 func _check_capacity():
 	var max = _game_manager._max_adventurers;
--- Scripts/UI/windows/window_base.gd
+++ Scripts/UI/windows/window_base.gd
@@ -11,26 +11,32 @@ const MARGINS_TOP = 4;
 @export var win_header_minimize: Button = null;
 var _key = "";
 var _manager: Node = null;
+var _instance: Control = null;
 
 func populate(key, content_obj, window_pos):
 	_key = key;
-	var instance: Panel = ResourceLoader.load(content_obj.content_ref).instantiate();
-	content_parent.add_child(instance);
-	var window_size = _calc_window_size(instance.get_global_rect());
+	_instance = ResourceLoader.load(content_obj.content_ref).instantiate();
+	content_parent.add_child(_instance);
+	var window_size = resize_app();
 	self.set_position(window_pos if window_pos != null else _calc_window_offset(position, window_size));
-	window_base.set_size(window_size);
 	win_header_title.text = content_obj.title;
 	win_header_close.visible = content_obj.has_close_btn if content_obj.has('has_close_btn') else true;
 	win_header_close.pressed.connect(_on_win_close);
 	win_header_minimize.visible = content_obj.has_minimize_btn if content_obj.has('has_minimize_btn') else false;
 	
-	var app_properties = instance.get_property_list().map(func (x): return x.name);
+	var app_properties = _instance.get_property_list().map(func (x): return x.name);
 	if app_properties.has('_window_base'):
-		instance._window_base = self;
+		_instance._window_base = self;
 	if app_properties.has('_on_close_action'):
-		win_header_close.pressed.connect(instance._on_close_action);
+		win_header_close.pressed.connect(_instance._on_close_action);
 
 	pass;
+
+func resize_app() -> Vector2:
+	var window_size = _calc_window_size(_instance.get_global_rect());
+	window_base.set_size(window_size);
+	return window_size;
+
 
 func _calc_window_size(content_rect: Rect2) -> Vector2:
 	return content_rect.size + Vector2(MARGINS_LRD, MARGINS_LRD) + Vector2(MARGINS_LRD, HEADER_HEIGHT + MARGINS_TOP);
