4d438fba495362dfccc71739eb75b6b8b6088f12
Momonyaro <58791263+Momonyaro@users.noreply.github.com>
2023-12-03 20:17:31 +0100

    fixed performance issue in adv_list
    I was stupid and was connecting the event every frame 🤦‍♂️

--- Scripts/UI/components/adv_list_item.gd
+++ Scripts/UI/components/adv_list_item.gd
@@ -1,18 +1,21 @@
 extends NinePatchRect
 
 var _game_manager : GameManager = null;
+var _id = "";
 @onready var content = $content;
 @onready var human_icon = ResourceLoader.load("res://Textures/Icons/human.png");
 @onready var demihuman_icon = ResourceLoader.load("res://Textures/Icons/demi-human.png");
 
 func _ready():
 	_game_manager = get_node("/root/Root/Game");
+	get_node("%content/select_btn").pressed.connect(_on_select_btn);
 
 func populate(adventurer: Adventurer):
 	content.visible = adventurer != null;
 	if !content.visible:
 		return;
 	
+	_id = adventurer._unique_id;
 	_draw_info(adventurer);
 	pass;
 		
@@ -24,4 +27,6 @@ func _draw_info(adventurer: Adventurer):
 	get_node("%content/race").tooltip_text = "Human" if is_human else "Demi-Human";
 	get_node("%content/level_race").text = str("Level ", adventurer._level, " ", adventurer._class);
 	get_node("%content/current/text").text = str("Currently: ", adventurer.adv_status());
-	get_node("%content/select_btn").pressed.connect(func(): _game_manager.select_agent.emit(adventurer._unique_id));
+
+func _on_select_btn():
+	_game_manager.select_agent.emit(_id);
