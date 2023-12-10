983c6ffcc5c4e6e3923af0f2dd47c316af0e208d
Momonyaro <58791263+Momonyaro@users.noreply.github.com>
2023-12-10 22:32:37 +0100

    moved guild name away from context menu
    Instead place it in the 'Guild' window that opens via clicking the section/the button under 'view'

--- Scripts/Guild/GuildData.gd
+++ Scripts/Guild/GuildData.gd
@@ -1,5 +1,5 @@
 class_name GuildData;
 
-var guild_name: String = "DEFAULT_NAME";
-var guild_level: int = 99;
-var guild_xp: Vector2i = Vector2i(500, 1000);
+var guild_name: String = "Den of Hungry Adventurers";
+var guild_level: int = 24;
+var guild_xp: Vector2i = Vector2i(24, 100);
--- Scripts/UI/GuildLevelInfo.gd
+++ Scripts/UI/GuildLevelInfo.gd
@@ -1,11 +1,9 @@
 extends HBoxContainer
 
-@export var name_text: Label;
 @export var level_text: Label;
 @export var xp_bar: ProgressBar;
 
 func populate(data: GuildData):
-	name_text.text = data.guild_name;
 	level_text.text = str(data.guild_level);
 	xp_bar.value = data.guild_xp.x / float(data.guild_xp.y);
 	
