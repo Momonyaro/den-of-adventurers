ab8f25cae3bddb018797517e2f8a123374aedd05
Momonyaro <58791263+Momonyaro@users.noreply.github.com>
2024-03-13 21:34:04 +0100

    added chair and dining table model

--- Scripts/UI/GuildLevelInfo.gd
+++ Scripts/UI/GuildLevelInfo.gd
@@ -3,6 +3,11 @@ extends HBoxContainer
 @export var level_text: Label;
 @export var xp_bar: ProgressBar;
 
+@onready var _game_manager : GameManager = get_node("/root/Root/Game");
+
+func _process(_delta):
+	populate(_game_manager.guild_data);
+
 func populate(data: GuildData):
 	level_text.text = str(data.guild_level);
 	xp_bar.value = data.guild_xp.x / float(data.guild_xp.y);
